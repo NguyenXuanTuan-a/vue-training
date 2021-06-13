49444f0c304f38e49feee1a3a3deef18c33e6702
tieudamtac <hieungochoangit@gmail.com>
2021-06-13 16:27:29 +0700

    Component project con

--- vue-hieuhn/training/src/views/project/ListProject.vue
+++ vue-hieuhn/training/src/views/project/ListProject.vue
@@ -2,26 +2,7 @@
   <div>
     <h1 class="project-header">Project List</h1>
     <v-row>
-      <v-col xl="2" v-for="(item, i) in listProject" :key="i">
-        <router-link
-          :to="{ name: 'DetailProject', params: { id: item.id, item: item } }"
-          class="projectItem"
-        >
-          <v-card class="mx-auto">
-            <v-card-text>
-              <h1 class="project-title">
-                {{ item.name }}
-                <span class="project-important" v-show="item.important">*</span>
-              </h1>
-              <div class="project-content">
-                <h4 class="project-auth">PM: {{ item.pm }}</h4>
-                <p class="bold">Teamsize: {{ item.teamSize }}</p>
-                <p class="bold">Khach hang: {{ item.customer }}</p>
-              </div>
-            </v-card-text>
-          </v-card>
-        </router-link>
-      </v-col>
+      <ProjectCon v-for="(item, i) in listProject" :key="i" :item="item"></ProjectCon>
     </v-row>
     <router-link class="home" to="/">
       <v-btn>Trang chu</v-btn>
@@ -31,8 +12,13 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import ProjectCon from './ProjectCon.vue'
 
-@Component
+@Component({
+  components: {
+    ProjectCon
+  }
+})
 export default class ListProject extends Vue {
   @Prop() private listProject?: any[];
 }
---
+++ vue-hieuhn/training/src/views/project/ProjectCon.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-col xl="2">
+        <router-link
+          :to="{ name: 'DetailProject', params: { id: item.id, item: item } }"
+          class="projectItem"
+        >
+          <v-card class="mx-auto">
+            <v-card-text>
+              <h1 class="project-title">
+                {{ item.name }}
+                <span class="project-important" v-show="item.important">*</span>
+              </h1>
+              <div class="project-content">
+                <h4 class="project-auth">PM: {{ item.pm }}</h4>
+                <p class="bold">Teamsize: {{ item.teamSize }}</p>
+                <p class="bold">Khach hang: {{ item.customer }}</p>
+              </div>
+            </v-card-text>
+          </v-card>
+        </router-link>
+      </v-col>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class ProjectCon extends Vue {
+    @Prop() private item?: any
+}
+</script>
+
+<style>
+    
+</style>
